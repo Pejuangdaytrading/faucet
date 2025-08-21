231dbc5a253c75864d59c360e709bfe671641b77
Pejuangdaytrading <pikipurwanto1@gmail.com>
2025-08-21 16:26:34 +0700

    Update bot.js

--- pages/api/bot.js
+++ pages/api/bot.js
@@ -1,24 +1,24 @@
+// pages/api/bot.js
 export default async function handler(req, res) {
   if (req.method === "POST") {
     const body = req.body;
 
-    // pastikan ada message dari Telegram
+    // pastikan ada message
     if (body.message && body.message.text === "/start") {
       const chatId = body.message.chat.id;
 
-      // balas pakai API Telegram
       await fetch(`https://api.telegram.org/bot${process.env.BOT_TOKEN}/sendMessage`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
           chat_id: chatId,
-          text: "🚀 Open Faucet",
+          text: "💎 Open Faucet",
           reply_markup: {
             keyboard: [[{ text: "💎 Open Faucet" }]],
             resize_keyboard: true,
-            one_time_keyboard: true
-          }
-        })
+            one_time_keyboard: true,
+          },
+        }),
       });
     }
 
